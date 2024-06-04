4fd0343e846e2d1ab545f7ebab44bab769b7d44f
Diwakar Singh <diwakar@heady.io>
2024-06-04 19:36:57 +0530

    Added s3 services for Get and Put Action

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,28 +1,69 @@
 const Joi = require("joi");
 const notesModel = require("../models/notesModel");
+const { putPreSignedUrl, getPreSignedUrl } = require("../services/awsServices");
+const { awsEnv } = require("../config/envConfig");
+const { createProduct } = require("./paymentController");
 
-module.exports.create = async (req,res) => {
-   const {title, cost, coverLocation,language,topics,pdfLocation,name} = req.body;
-   const check = {
-      coverLocation: Joi.string().required().min(4).max(256),
-      language: Joi.string().required().min(4).max(256),
-      topics: Joi.array(),
-      pdfLocation: Joi.string().required().min(4).max(256),
-      name: Joi.string().required().min(4).max(256),
-      title: Joi.string().required().min(4).max(256),
-      cost: Joi.number().required().greater(0)
-   }
-   const {error} = Joi.valid({ title, cost, coverLocation, language, topics, pdfLocation, name },check)
-   if(error) {
-      res.status(400).send({'error': error})
-   } 
-   const notesDoc = new notesModel({title, cost, coverLocation,language,topics,pdfLocation,name})
-   const doc = await notesDoc.save()
+module.exports.create = async (req, res) => {
+  const { title, cost, coverLocation, language, topics, pdfLocation, name } =
+    req.body;
+  const check = {
+    language: Joi.string().required().min(4).max(256),
+    topics: Joi.array(),
+    name: Joi.string().required().min(4).max(256),
+    coverLocation: Joi.string().required().min(4).max(256),
+    pdfLocation: Joi.string().required().min(4).max(256),
+    title: Joi.string().required().min(4).max(256),
+    cost: Joi.number().required().greater(0),
+  };
+  const { error } = Joi.valid(
+    { title, cost, coverLocation, language, topics, pdfLocation, name },
+    check
+  );
+  if (error) {
+    res.status(400).send(error);
+  }
+  // if successful, update stripe services
+  try {
+    createProduct(name, cost);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+  // Update mongoose db
+  try {
+    const notesDoc = new notesModel({
+      title,
+      cost,
+      coverLocation,
+      language,
+      topics,
+      pdfLocation,
+      name,
+    });
+    const doc = await notesDoc.save();
 
-   res.status(200).json({createdDoc: doc});
-}
+    res.status(200).json({ createdDoc: doc });
+  } catch (error) {
+    // delete the stripe products
+  }
+};
 
 module.exports.getAll = async (req, res) => {
-console.log('Per',(notesModel))
-   res.status(200).json(notesModel);
-}
+  console.log("Per", notesModel);
+  res.status(200).json(notesModel);
+};
+
+module.exports.getSignedUrl = async (req, res) => {
+  try {
+    const { type, name, action } = req.body;
+    if (action === "get") {
+      const url = getPreSignedUrl(awsEnv.notesBucketName, `${type}/${name}`);
+    } else if (action === "put") {
+      const url = putPreSignedUrl(awsEnv.notesBucketName, `${type}/${name}`);
+    }
+
+    req.status(200).json({ url });
+  } catch (error) {
+    req.status(400).send(error);
+  }
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const notesController = require('../controllers/notesController')
 
 router.get('/', notesController.getAll);
+router.post('/getSignedUrl',notesController.getSignedUrl)
 router.post('/create',notesController.create);
 router.post('/checkout',()=>console.log('checkout'))
 
